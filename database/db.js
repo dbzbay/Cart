3438cf4066e0d9f288eeaf43396d52ad3f9e8080
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-29 10:47:10 -0500

    seeded and linted both mongoDB and postgreSQL

--- database/db.js
+++ database/db.js
@@ -13,4 +13,45 @@ const itemsSchema = mongoose.Schema({
 
 const Item = mongoose.model('Item', itemsSchema);
 
-module.exports = { Item };
+//console.log(Item);
+
+module.exports = { Item, Kitten, kittySchema };
+
+/******/
+//var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var kittySchema = new mongoose.Schema({
+  name: String
+});
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+var fluffy = new Kitten({ name: 'scruffy' });
+fluffy.speak(); // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+  fluffy.speak();
+});
+
+Kitten.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens);
+})
